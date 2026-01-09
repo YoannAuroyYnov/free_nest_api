42150e111ea334d4071f44c0889c7d6a75cb9f9c
Yoann Auroy <yoann.ar.pro@gmail.com>
2026-01-09 16:32:58 +0100

    feat: add google auth login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Req,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { Request } from 'express';
+import { GoogleAuthUser } from './dto/google-auth-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +29,16 @@ export class AuthController {
   @HttpCode(201)
   signup(@Body() signUpDto: SignUpDto): Promise<void> {
     return this.authService.signup(signUpDto);
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(@Req() req: Request) {
+    const { user } = req;
+    return this.authService.googleLogin(user as GoogleAuthUser);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { UserService } from 'src/user/user.service';
 import { SecurityService } from 'src/common/security/security.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
+import { GoogleAuthUser } from './dto/google-auth-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -37,6 +38,29 @@ export class AuthService {
       if (error instanceof Error)
         throw new UnauthorizedException('Invalid email or password');
     }
+  }
+
+  async googleLogin(googleUser: GoogleAuthUser) {
+    const { googleId, email, firstName } = googleUser;
+
+    let user = await this.usersService.findByProviderId('google', googleId);
+
+    if (!user) {
+      user = await this.usersService.create({
+        email,
+        firstName,
+        providerId: googleId,
+        password: Math.random().toString(36).slice(-8),
+      });
+    }
+
+    const { accessToken } = await this.securityService.generateAccessToken({
+      userId: user.id,
+      email,
+      firstName,
+    });
+
+    return { accessToken, user: googleUser };
   }
 
   async signup(signUpDto: SignUpDto) {
